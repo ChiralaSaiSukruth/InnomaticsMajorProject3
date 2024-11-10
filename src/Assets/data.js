ba3eba5a331472fb846c22b6bfca12ffced06f83
--saisukruth <sukruth.chirala@gmail.com>
2024-11-10 09:19:01 +0530

    codepushed

---
+++ src/Assets/data.js
@@ -0,0 +1,44 @@
+export const data =[
+  
+    {
+        question:"What is UI?",
+        Option1:"User Interface",
+        Option2:"User Experience",
+        Option3:"Wire frame",
+        Option4:"Prototyping",
+        ans:1,
+},
+{
+    question:"What is UX?",
+    Option1:"User Interface",
+    Option2:"User Experience",
+    Option3:"Wire Frame",
+    Option4:"None of above ",
+    ans:2,
+},
+{
+    question:"What are  software tools using for UI/UX designing",
+    Option1:"Figma",
+    Option2:"Adobe XD",
+    Option3:"Sketch",
+    Option4:"All of the above ",
+    ans:4,
+},
+{
+    question:"What is Wire frame?",
+    Option1:"Low-fedility",
+    Option2:"High-fedility",
+    Option3:"prototyping",
+    Option4:"A wireframe is a visual blueprint that represents the basic structure and layout of a webpage, app, or other interface. ",
+    ans:4,
+},
+{
+    question:"What is Low-fedility design in wireframe",
+    Option1:"Low-fidelity (low-fi) design is an early, simplified version of a design that focuses on essential elements and functionality without adding detailed visuals, colors, or refined graphics.",
+    Option2:"High-fidelity (high-fi) design is a detailed, polished representation of a digital product that closely resembles the final product in terms of visuals, functionality, and interactivity. ",
+    Option3:"Wireframe",
+    Option4:"Prototype",
+    ans:1,
+},
+
+];
